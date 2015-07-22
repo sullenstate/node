7859010b624710a360ced8bc4dffb2784ab9d4ee
Tim <tklein@outlook.com>
2015-07-22 11:35:18 -0600

    Final Commit

--- node3/app.js
+++ node3/app.js
@@ -8,7 +8,22 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', indexController.index);
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
+app.post('/formsubmit', function (req, res) {
+	console.log(req.body)
+    res.redirect('/success')
+});
+
+app.get('/formsubmit', function (req, res) {
+	res.render('index');
+});
+
+app.get('/success', function (req, res) {
+	res.send('Success!')
+});
 
 var server = app.listen(5698, function() {
 	console.log('Express server listening on port ' + server.address().port);
